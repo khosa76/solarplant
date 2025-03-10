16b7f4520c8b63019e8434278d5c894376fec228
Jaskaran Singh Khosa <84892564+khosa76@users.noreply.github.com>
2025-03-10 23:42:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,332 +1,321 @@
-var _0x375aa2 = _0xf2c3;
-(function (_0x42f833, _0xe662e1) {
-  var _0x53031b = _0xf2c3,
-    _0x17b799 = _0x42f833();
-  while (!![]) {
-    try {
-      var _0x1167e3 =
-        parseInt(_0x53031b(0x1cd)) / 0x1 +
-        parseInt(_0x53031b(0x1ec)) / 0x2 +
-        parseInt(_0x53031b(0x1d2)) / 0x3 +
-        parseInt(_0x53031b(0x1d7)) / 0x4 +
-        parseInt(_0x53031b(0x1f3)) / 0x5 +
-        -parseInt(_0x53031b(0x1bd)) / 0x6 +
-        -parseInt(_0x53031b(0x1bc)) / 0x7;
-      if (_0x1167e3 === _0xe662e1) break;
-      else _0x17b799["push"](_0x17b799["shift"]());
-    } catch (_0x1a5442) {
-      _0x17b799["push"](_0x17b799["shift"]());
-    }
-  }
-})(_0x1be0, 0x917cd);
-let globalUsr = "",
-  globalPowSha1 = "",
-  globalJsonResponse = {},
-  globalurl = "",
-  globalMonthUrl = "",
-  HTTP_INTERFACE_ADDRESS_NEW = "https://web.shinemonitor.com/public/";
-function login(_0x2a4e86, _0x2850f4) {
-  var _0x149ef7 = _0xf2c3;
-  (globalPowSha1 = sha1(_0x2850f4)),
-    console["log"](_0x149ef7(0x1f2), globalPowSha1),
-    (globalUsr = _0x2a4e86),
-    console[_0x149ef7(0x1f4)](_0x149ef7(0x1b6), _0x2a4e86);
+// const sha1 = require('js-sha1');
+// const axios = require('axios');
+
+// Global variables to store the username, hashed password, and response
+let globalUsr = "";
+let globalPowSha1 = "";
+let globalJsonResponse = {}; // Variable to store the JSON response
+let globalurl = ""; // Variable to store the final URL
+let globalMonthUrl = "";
+let HTTP_INTERFACE_ADDRESS_NEW = "https://web.shinemonitor.com/public/";
+let isDataLoaded = false;
+
+// Login function
+function login(usr, pwd) {
+  // Hash the password using sha1 package
+  globalPowSha1 = sha1(pwd); // SHA-1 hash of password
+  console.log("Global Hashed created :", globalPowSha1);
+
+  globalUsr = usr;
+  console.log("Global user created :", usr);
 }
-async function http_login(_0x232be2, _0x156802) {
-  var _0x35b93b = _0xf2c3,
-    _0x15846e = new Date()[_0x35b93b(0x1bf)](),
-    _0x316f6d =
-      _0x35b93b(0x1b9) +
-      encodeURI(_0x232be2)
-        [_0x35b93b(0x1bb)]("+", _0x35b93b(0x1e1))
-        [_0x35b93b(0x1bb)]("\x27", "%27") +
-      _0x35b93b(0x1be),
-    _0x259468 = sha1(_0x15846e + _0x156802 + _0x316f6d),
-    _0x5c7fb9 =
-      _0x35b93b(0x1e3) +
-      _0x35b93b(0x1da) +
-      _0x259468 +
-      _0x35b93b(0x1dc) +
-      _0x15846e +
-      _0x316f6d;
-  console[_0x35b93b(0x1f4)](_0x35b93b(0x1c3), _0x5c7fb9);
+
+// HTTP login function
+async function http_login(usr, pwdSha1) {
   try {
-    const _0x2feb99 = await axios[_0x35b93b(0x1b4)](_0x5c7fb9);
-    (globalJsonResponse = _0x2feb99["data"]),
-      console[_0x35b93b(0x1f4)](_0x35b93b(0x1e2), globalJsonResponse),
-      getUrl();
-  } catch (_0x2a617b) {
-    console[_0x35b93b(0x1c5)](_0x35b93b(0x1d6), _0x2a617b);
+    // Show loading indicator
+    showLoadingState(true);
+    
+    var salt = new Date().getTime();
+    var action =
+      "&action=auth&usr=" +
+      encodeURI(usr).replace("+", "%2B").replace("'", "%27") +
+      "&company-key=bnrl_frRFjEz8Mkn";
+    var sign = sha1(salt + pwdSha1 + action); // Use sha1 package instead of hex_sha1
+    var url =
+      "https://web.shinemonitor.com/public/" +
+      "?sign=" +
+      sign +
+      "&salt=" +
+      salt +
+      action; // Example URL
+
+    console.log("Generated URL: ", url); // Log the URL to the console
+
+    // Make the HTTP request and get the response
+    const response = await axios.get(url);
+
+    // Store the JSON response
+    globalJsonResponse = response.data;
+
+    // Log the JSON response to the console
+    console.log("Received JSON Response:", globalJsonResponse);
+    getUrl();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    // Show error to user
+    showErrorMessage();
   }
 }
+
 function getUrl() {
-  var _0x4d011b = _0xf2c3,
-    _0x5ed522 = "",
-    _0x552226 = _0x4d011b(0x1cf),
-    _0x3f2ace =
-      _0x4d011b(0x1e7) +
-      _0x4d011b(0x1e8) +
-      _0x4d011b(0x1e0) +
-      _0x5ed522 +
-      _0x4d011b(0x1c9) +
-      _0x4d011b(0x1b8),
-    _0x1f29cf = globalJsonResponse[_0x4d011b(0x1d5)],
-    _0x4268bc = new Date()[_0x4d011b(0x1bf)](),
-    _0x1bf915 = sha1(
-      _0x4268bc + _0x1f29cf[_0x4d011b(0x1d1)] + _0x1f29cf["token"] + _0x3f2ace
-    ),
-    _0x4374e5 =
-      _0x4d011b(0x1e3) +
-      _0x4d011b(0x1da) +
-      _0x1bf915 +
-      _0x4d011b(0x1dc) +
-      _0x4268bc +
-      _0x4d011b(0x1b7) +
-      _0x1f29cf[_0x4d011b(0x1b5)] +
-      _0x3f2ace +
-      _0x552226;
-  (globalurl = _0x4374e5),
-    console[_0x4d011b(0x1f4)](""),
-    console[_0x4d011b(0x1f4)](_0x4d011b(0x1c1), globalurl),
-    monthTab();
+  var flag = "";
+  var callfront = "&callback=Khosa";
+  var action =
+    "&action=queryPlantCurrentData&plantid=" +
+    "1286812" +
+    "&par=ENERGY_TODAY,ENERGY_MONTH,ENERGY_YEAR,ENERGY_TOTAL,ENERGY_PROCEEDS,ENERGY_CO2,CURRENT_TEMP,CURRENT_RADIANT,BATTERY_SOC,ENERGY_COAL,ENERGY_SO2" +
+    flag +
+    "&i18n=en_US" +
+    "&lang=en_US";
+  var currUsr = globalJsonResponse.dat;
+  var salt = new Date().getTime();
+
+  var sign = sha1(salt + currUsr.secret + currUsr.token + action);
+  var urlfinal =
+    "https://web.shinemonitor.com/public/" +
+    "?sign=" +
+    sign +
+    "&salt=" +
+    salt +
+    "&token=" +
+    currUsr.token +
+    action +
+    callfront;
+
+  globalurl = urlfinal;
+
+  console.log("");
+  console.log("2nd Generated URL: ", globalurl);
+  monthTab();
 }
+
 function fetchData() {
-  var _0x2d925f = _0xf2c3;
   if (!globalurl) {
-    console[_0x2d925f(0x1c5)](_0x2d925f(0x1e6));
+    console.error("globalurl is not set yet!");
     return;
   }
-  const _0x2d2f35 = document[_0x2d925f(0x1c8)](_0x2d925f(0x1dd));
-  (_0x2d2f35[_0x2d925f(0x1ed)] = globalurl),
-    console[_0x2d925f(0x1f4)](_0x2d925f(0x1ce)),
-    document[_0x2d925f(0x1cc)][_0x2d925f(0x1d3)](_0x2d2f35);
+
+  showLoadingState(true);
+  
+  const script = document.createElement("script");
+  script.src = globalurl; // Use the dynamically set globalurl
+  console.log("Fetched Refreshed");
+  document.body.appendChild(script);
 }
-function _0x1be0() {
-  var _0x2e9444 = [
-    "mti4nJGXmG",
-    "m3jKifvYBa",
-    "zMLUza",
-    "ru5fuKDzx1rpvefm",
-    "mtm5mdmZngTcEMHPwG",
-    "C3jJ",
-    "i2u4zJvLoq",
-    "zw5LCMD5lw1VBNrOlte",
-    "z2v0rNvSBfLLyxi",
-    "z2v0rwXLBwvUDej5swq",
-    "r2XVyMfSieHHC2HLzcbJCMvHDgvKidO",
-    "ndaZmtaWnvzQrgTVuW",
-    "Bg9N",
-    "z2v0",
-    "Dg9Rzw4",
-    "r2XVyMfSihvZzxiGy3jLyxrLzca6",
-    "jNrVA2vUpq",
-    "jMXHBMC9zw5Fvvm",
-    "jMfJDgLVBJ1HDxrOjNvZCJ0",
-    "jMrHDgu9",
-    "CMvWBgfJzq",
-    "mti1odi3ndvtthj3BLa",
-    "nJe4oty4ngfwBLftuW",
-    "jMnVBxbHBNKTA2v5pwjUCMXFzNjsrMPfEJHnA24",
-    "z2v0vgLTzq",
-    "igTxAa",
-    "mM5KieDLBMvYyxrLzcbvuKW6ia",
-    "C2nYB2XSvg8",
-    "r2vUzxjHDgvKifvstdOG",
-    "jti3",
-    "zxjYB3i",
-    "Aw5UzxjuzxH0",
-    "jtiW",
-    "y3jLyxrLrwXLBwvUDa",
-    "jMKXog49zw5Fvvm",
-    "BMv4Dc1IDg4Tmq",
-    "y29SB3i",
-    "yM9KEq",
-    "ndKYnJG0Ew1hyLrp",
-    "rMv0y2HLzcbszwzYzxnOzwq",
-    "jMnHBgXIywnRpuTOB3nH",
-    "BMv4Dc1IDg4",
-    "C2vJCMv0",
-    "mti4ndCYmhjZvMLmwq",
-    "yxbWzw5Kq2HPBgq",
-    "zw5LCMD5lw1VBNrO",
-    "zgf0",
-    "rxjYB3iGzMv0y2HPBMCGzgf0ytO",
-    "ndaXmta5nMn4z3HYtW",
-    "z2v0tw9UDgG",
-    "DMfS",
-    "p3nPz249",
-    "jtiZ",
-    "jNnHBhq9",
-    "C2nYAxb0",
-    "s2HVC2e3nZy",
-    "B25SB2fK",
-    "jNbHCJ1ftKvsr1LFve9eqvKSru5fuKDzx01ptLrilevorvjhwv9zrufslevorvjhwv9ut1rbtcXftKvsr1LFufjpq0vfrfmSru5fuKDzx0npmIXdvvjsru5ux1rftvaSq1vsuKvovf9squrjqu5ulejbvfrfuLLFu09dlevorvjhwv9dt0fmlevorvjhwv9ttZi",
-    "jtjc",
-    "uMvJzwL2zwqGsLnptIbszxnWB25ZztO",
-    "Ahr0Chm6lY93zwiUC2HPBMvTB25PDg9YlMnVBs9WDwjSAwmV",
-    "C3r5Bgu",
-    "A2v5",
-    "z2XVyMfSDxjSigLZig5VDcbZzxqGEwv0iq",
-    "jMfJDgLVBJ1XDwvYEvbSyw50q3vYCMvUDerHDgeMCgXHBNrPzd0",
-  ];
-  _0x1be0 = function () {
-    return _0x2e9444;
-  };
-  return _0x1be0();
+
+function Khosa(data) {
+  showLoadingState(false);
+  
+  try {
+    const energyToday = parseFloat(
+      data.dat.find((item) => item.key === "ENERGY_TODAY").val
+    ).toFixed(1);
+    const energyMonth = parseFloat(
+      data.dat.find((item) => item.key === "ENERGY_MONTH").val
+    ).toFixed(1);
+    const energyTotal = parseFloat(
+      data.dat.find((item) => item.key === "ENERGY_TOTAL").val
+    ).toFixed(1);
+
+    // Update the values and hide the spinners
+    updateDataValue("energy-today", `${energyToday}`);
+    updateDataValue("energy-month-1", `${energyToday}`);
+    updateDataValue("energy-month", `${energyMonth}`);
+    updateDataValue("energy-total", `${energyTotal}`);
+    
+    isDataLoaded = true;
+  } catch (error) {
+    console.error("Error processing data:", error);
+    showErrorMessage();
+  }
 }
-function Khosa(_0x1ffa53) {
-  var _0x2f35d9 = _0xf2c3;
-  const _0x737b32 = parseFloat(
-      _0x1ffa53[_0x2f35d9(0x1d5)][_0x2f35d9(0x1ea)](
-        (_0x4cbd1b) => _0x4cbd1b[_0x2f35d9(0x1e5)] === "ENERGY_TODAY"
-      )[_0x2f35d9(0x1d9)]
-    )["toFixed"](0x1),
-    _0x936306 = parseFloat(
-      _0x1ffa53[_0x2f35d9(0x1d5)]["find"](
-        (_0x1b49a2) => _0x1b49a2[_0x2f35d9(0x1e5)] === "ENERGY_MONTH"
-      )[_0x2f35d9(0x1d9)]
-    )["toFixed"](0x1),
-    _0x13bd69 = parseFloat(
-      _0x1ffa53[_0x2f35d9(0x1d5)]["find"](
-        (_0x2a8eb5) => _0x2a8eb5["key"] === _0x2f35d9(0x1eb)
-      )[_0x2f35d9(0x1d9)]
-    )["toFixed"](0x1);
-  (document[_0x2f35d9(0x1f1)]("energy-today")[_0x2f35d9(0x1c6)] =
-    _0x737b32 + _0x2f35d9(0x1c0)),
-    (document[_0x2f35d9(0x1f1)](_0x2f35d9(0x1ef))[_0x2f35d9(0x1c6)] =
-      _0x737b32 + _0x2f35d9(0x1c0)),
-    (document[_0x2f35d9(0x1f1)](_0x2f35d9(0x1d4))["innerText"] =
-      _0x936306 + _0x2f35d9(0x1c0)),
-    (document[_0x2f35d9(0x1f1)]("energy-total")[_0x2f35d9(0x1c6)] =
-      _0x13bd69 + "\x20kWh");
+
+// Function to update a data value and hide its spinner
+function updateDataValue(elementId, value) {
+  const element = document.getElementById(elementId);
+  
+  // Remove any existing spinners
+  const spinner = element.querySelector('.loading-spinner');
+  if (spinner) {
+    spinner.remove();
+  }
+  
+  // Set the text value
+  element.textContent = value;
 }
-function _0xf2c3(_0x11ff13, _0x56a1a6) {
-  var _0x1be072 = _0x1be0();
-  return (
-    (_0xf2c3 = function (_0xf2c3c0, _0x1b3e1c) {
-      _0xf2c3c0 = _0xf2c3c0 - 0x1b4;
-      var _0x50c017 = _0x1be072[_0xf2c3c0];
-      if (_0xf2c3["jMfYEu"] === undefined) {
-        var _0x56f029 = function (_0x2a4e86) {
-          var _0x2850f4 =
-            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
-          var _0x232be2 = "",
-            _0x156802 = "";
-          for (
-            var _0x15846e = 0x0, _0x316f6d, _0x259468, _0x5c7fb9 = 0x0;
-            (_0x259468 = _0x2a4e86["charAt"](_0x5c7fb9++));
-            ~_0x259468 &&
-            ((_0x316f6d =
-              _0x15846e % 0x4 ? _0x316f6d * 0x40 + _0x259468 : _0x259468),
-            _0x15846e++ % 0x4)
-              ? (_0x232be2 += String["fromCharCode"](
-                  0xff & (_0x316f6d >> ((-0x2 * _0x15846e) & 0x6))
-                ))
-              : 0x0
-          ) {
-            _0x259468 = _0x2850f4["indexOf"](_0x259468);
-          }
-          for (
-            var _0x2feb99 = 0x0, _0x2a617b = _0x232be2["length"];
-            _0x2feb99 < _0x2a617b;
-            _0x2feb99++
-          ) {
-            _0x156802 +=
-              "%" +
-              ("00" + _0x232be2["charCodeAt"](_0x2feb99)["toString"](0x10))[
-                "slice"
-              ](-0x2);
-          }
-          return decodeURIComponent(_0x156802);
-        };
-        (_0xf2c3["LnotwE"] = _0x56f029),
-          (_0x11ff13 = arguments),
-          (_0xf2c3["jMfYEu"] = !![]);
-      }
-      var _0x5d8951 = _0x1be072[0x0],
-        _0xed678f = _0xf2c3c0 + _0x5d8951,
-        _0x1433a7 = _0x11ff13[_0xed678f];
-      return (
-        !_0x1433a7
-          ? ((_0x50c017 = _0xf2c3["LnotwE"](_0x50c017)),
-            (_0x11ff13[_0xed678f] = _0x50c017))
-          : (_0x50c017 = _0x1433a7),
-        _0x50c017
-      );
-    }),
-    _0xf2c3(_0x11ff13, _0x56a1a6)
-  );
+
+function showLoadingState(isLoading) {
+  if (isLoading && !isDataLoaded) {
+    // Show spinners for each data element
+    showSpinner("energy-month-1");
+    showSpinner("energy-month");
+    showSpinner("energy-total");
+    
+    // Optional: add visual loading indicators here
+    const cards = document.querySelectorAll(".data-card, .data-card-2");
+    cards.forEach(card => {
+      card.style.opacity = "0.9";
+    });
+  } else if (!isLoading) {
+    // Remove visual loading indicators
+    const cards = document.querySelectorAll(".data-card, .data-card-2");
+    cards.forEach(card => {
+      card.style.opacity = "1";
+    });
+  }
 }
-function disableUserInteractions() {}
+
+// Function to show the spinner in a data element
+function showSpinner(elementId) {
+  const element = document.getElementById(elementId);
+  
+  // Clear any existing content
+  element.innerHTML = '';
+  
+  // Create and append the spinner
+  const spinner = document.createElement('div');
+  spinner.className = 'loading-spinner';
+  element.appendChild(spinner);
+}
+
+function showErrorMessage() {
+  const errorMessage = "ਡਾਟਾ ਲੋਡ ਨਹੀਂ ਹੋ ਸਕਿਆ";
+  
+  updateDataValue("energy-month-1", errorMessage);
+  updateDataValue("energy-month", errorMessage);
+  updateDataValue("energy-total", errorMessage);
+  
+  // Add a retry button or auto-retry
+  setTimeout(() => {
+    if (!isDataLoaded) {
+      fetchData();
+    }
+  }, 5000);
+}
+
+function disableUserInteractions() {
+  // document.addEventListener("touchmove", (event) => event.preventDefault(), { passive: false });
+  // document.addEventListener("gesturestart", (event) => event.preventDefault(), { passive: false });
+  // document.addEventListener("touchstart", (event) => event.preventDefault(), { passive: false });
+  // document.addEventListener("keydown", (event) => event.preventDefault());
+}
+
 function monthTab() {
-  PLANT_MONTH_STATE = ![];
-  let _0xccc183 = "";
-  _0x49c5d5();
-  function _0x49c5d5() {
-    var _0x5204a5 = _0xf2c3,
-      _0x574cfc = new Date();
-    THIS_MONTH = parseDate2yyyymm(_0x574cfc);
-    var _0x5c38aa = parseDate2yyyymm(_0x574cfc);
-    let _0x3951c0 = _0x5204a5(0x1e8);
-    _0xf71cd4(_0x3951c0, THIS_MONTH);
+  PLANT_MONTH_STATE = false;
+  let MAIN_CHOOSE_PLANT_ID = "";
+  thisMonthData();
+
+  function thisMonthData() {
+    var d = new Date();
+    THIS_MONTH = parseDate2yyyymm(d);
+    var monthdate = parseDate2yyyymm(d);
+
+    let MAIN_CHOOSE_PLANT_ID = "1286812";
+    getMonthData(MAIN_CHOOSE_PLANT_ID, THIS_MONTH);
   }
-  function _0xf71cd4(_0x427385, _0x432874) {
-    var _0x315230 = _0xf2c3,
-      _0x46dbe2 =
-        "&action=queryPlantEnergyMonthPerDay&plantid=" +
-        _0x427385 +
-        _0x315230(0x1ba) +
-        _0x432874;
-    http_async_request_public(_0x46dbe2);
+
+  function getMonthData(MAIN_CHOOSE_PLANT_ID, date) {
+    var monthUrl =
+      "&action=queryPlantEnergyMonthPerDay&plantid=" +
+      MAIN_CHOOSE_PLANT_ID +
+      "&date=" +
+      date;
+    http_async_request_public(monthUrl);
   }
 }
-function http_async_request_public(_0x3958bd, _0x48889e, _0x4e775d) {
-  var _0x2b94a7 = _0xf2c3;
-  _0x3958bd = _0x3958bd + "&i18n=en_US" + _0x2b94a7(0x1b8);
-  var _0x22a7e4 = globalJsonResponse[_0x2b94a7(0x1d5)],
-    _0x4742eb = new Date()["getTime"](),
-    _0x56ccd9 = sha1(
-      _0x4742eb +
-        _0x22a7e4[_0x2b94a7(0x1d1)] +
-        _0x22a7e4[_0x2b94a7(0x1b5)] +
-        _0x3958bd[_0x2b94a7(0x1bb)](/#/g, _0x2b94a7(0x1db))
-          [_0x2b94a7(0x1bb)](/'/g, _0x2b94a7(0x1c4))
-          [_0x2b94a7(0x1bb)](/ /g, "%20")
-    ),
-    _0x26bb1c =
-      HTTP_INTERFACE_ADDRESS_NEW +
-      _0x2b94a7(0x1da) +
-      _0x56ccd9 +
-      _0x2b94a7(0x1dc) +
-      _0x4742eb +
-      _0x2b94a7(0x1b7) +
-      _0x22a7e4[_0x2b94a7(0x1b5)] +
-      _0x3958bd[_0x2b94a7(0x1bb)](/#/g, "%23")
-        [_0x2b94a7(0x1bb)](/'/g, _0x2b94a7(0x1c4))
-        [_0x2b94a7(0x1bb)](/ /g, _0x2b94a7(0x1c7));
-  (globalMonthUrl = _0x26bb1c),
-    console["log"](""),
-    console[_0x2b94a7(0x1f4)](_0x2b94a7(0x1e9), globalMonthUrl),
-    fetchMonthData();
+
+function http_async_request_public(action, onSuccess, onError) {
+  action = action + "&i18n=en_US" + "&lang=en_US";
+
+  var currUsr = globalJsonResponse.dat;
+
+  var salt = new Date().getTime();
+  var sign = sha1(
+    salt +
+      currUsr.secret +
+      currUsr.token +
+      action.replace(/#/g, "%23").replace(/'/g, "%27").replace(/ /g, "%20")
+  );
+
+  var url2 =
+    HTTP_INTERFACE_ADDRESS_NEW +
+    "?sign=" +
+    sign +
+    "&salt=" +
+    salt +
+    "&token=" +
+    currUsr.token +
+    action.replace(/#/g, "%23").replace(/'/g, "%27").replace(/ /g, "%20");
+
+  globalMonthUrl = url2;
+  console.log("");
+  console.log("3rd Url", globalMonthUrl);
+  fetchMonthData();
 }
-function parseDate2yyyymm(_0x1e281c) {
-  var _0x25dad0 = _0xf2c3,
-    _0x50cf03 = _0x1e281c[_0x25dad0(0x1f0)](),
-    _0x452b03 = _0x1e281c[_0x25dad0(0x1d8)]() + 0x1;
-  return _0x50cf03 + "-" + (_0x452b03 < 0xa ? "0" + _0x452b03 : _0x452b03);
+
+function parseDate2yyyymm(date) {
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  return y + "-" + (m < 10 ? "0" + m : m);
 }
-window[_0x375aa2(0x1df)] = async () => {
-  var _0x3b6466 = _0x375aa2;
-  login(_0x3b6466(0x1de), "stmyoB@86"),
-    await http_login(globalUsr, globalPowSha1),
-    (document[_0x3b6466(0x1f1)](_0x3b6466(0x1d0))[_0x3b6466(0x1e4)]["color"] =
-      _0x3b6466(0x1ee)),
-    (document[_0x3b6466(0x1f1)](_0x3b6466(0x1ca))[_0x3b6466(0x1e4)][
-      _0x3b6466(0x1cb)
-    ] = _0x3b6466(0x1ee)),
-    fetchData(),
-    yearTab(),
-    window[_0x3b6466(0x1c2)](0x0, 0x1),
-    disableUserInteractions(),
-    setInterval(fetchData, 0x493e0);
+
+// Add vibration feedback for button presses if supported by the device
+function vibrateOnTouch() {
+  if ('vibrate' in navigator) {
+    navigator.vibrate(50); // 50ms vibration for feedback
+  }
+}
+
+// Handle network connection status
+function handleConnectionStatus() {
+  const updateConnectionStatus = () => {
+    if (navigator.onLine) {
+      console.log('Back online, refreshing data...');
+      if (isDataLoaded === false) {
+        fetchData();
+      }
+    } else {
+      console.log('Offline');
+      // Could show an offline message here
+    }
+  };
+  
+  window.addEventListener('online', updateConnectionStatus);
+  window.addEventListener('offline', updateConnectionStatus);
+}
+
+window.onload = async () => {
+
+  document.getElementById("next-btn").style.color = "#e8f5e9";
+  document.getElementById("next-btn-1").style.color = "#e8f5e9";
+  document.getElementById("next-btn").style.opacity = "0";
+  document.getElementById("next-btn-1").style.opacity = "0";
+
+
+  // Setup initial loading state
+  showLoadingState(true);
+  
+  // Initialize connection handling
+  handleConnectionStatus();
+  
+  // Add touch feedback
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => {
+    button.addEventListener('touchstart', vibrateOnTouch);
+  });
+  
+  // Call the login function first and then fetch data
+  login("Khosa776", "stmyoB@86");
+  await http_login(globalUsr, globalPowSha1); // Ensure http_login has run before fetching data
+  
+  // Update button appearance
+
+
+  
+  fetchData(); // Call fetchData after globalurl is set
+  yearTab();
+  
+  // Periodically refresh data, but not too frequently to save battery
+  setInterval(fetchData, 600000); // 10 minutes
 };
