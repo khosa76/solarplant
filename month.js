0360653a0c60cd5bb550b4348b8e16f77eb96ea1
Jaskaran Singh Khosa <84892564+khosa76@users.noreply.github.com>
2025-03-10 23:43:11 +0530

    Update month.js

--- month.js
+++ month.js
@@ -1,274 +1,181 @@
-(function (_0x3d5ffd, _0x562613) {
-  const _0x37d513 = _0x33d0,
-    _0x3174d7 = _0x3d5ffd();
-  while (!![]) {
-    try {
-      const _0x50e38f =
-        -parseInt(_0x37d513(0x105)) / 0x1 +
-        (parseInt(_0x37d513(0xfa)) / 0x2) *
-          (-parseInt(_0x37d513(0x11b)) / 0x3) +
-        (-parseInt(_0x37d513(0x118)) / 0x4) *
-          (-parseInt(_0x37d513(0x10a)) / 0x5) +
-        -parseInt(_0x37d513(0x10d)) / 0x6 +
-        (parseInt(_0x37d513(0x122)) / 0x7) *
-          (-parseInt(_0x37d513(0xf1)) / 0x8) +
-        parseInt(_0x37d513(0xed)) / 0x9 +
-        parseInt(_0x37d513(0x109)) / 0xa;
-      if (_0x50e38f === _0x562613) break;
-      else _0x3174d7["push"](_0x3174d7["shift"]());
-    } catch (_0x5499a1) {
-      _0x3174d7["push"](_0x3174d7["shift"]());
+function monthTab() {
+    PLANT_MONTH_STATE = false;
+    let MAIN_CHOOSE_PLANT_ID = "";
+    thisMonthData();
+  
+    function thisMonthData() {
+      var d = new Date();
+      THIS_MONTH = parseDate2yyyymm(d);
+      var monthdate = parseDate2yyyymm(d);
+  
+      let MAIN_CHOOSE_PLANT_ID = "1286812";
+      getMonthData(MAIN_CHOOSE_PLANT_ID, THIS_MONTH);
+    }
+  
+    function getMonthData(MAIN_CHOOSE_PLANT_ID, date) {
+      var monthUrl =
+        "&action=queryPlantEnergyMonthPerDay&plantid=" +
+        MAIN_CHOOSE_PLANT_ID +
+        "&date=" +
+        date;
+      http_async_request_public(monthUrl);
     }
   }
-})(_0x88cb, 0x33b79);
-function _0x88cb() {
-  const _0x1f0cd2 = [
-    "jMrHDgu9",
-    "C2vJCMv0",
-    "ndm3ntu3mffjufDowq",
-    "mJCXoty1uxbqAMTv",
-    "zxjYB3i",
-    "iZaWnZnLnG",
-    "mty3mJC0t1foA0PM",
-    "jtiZ",
-    "CMvWBgfJzq",
-    "jMKXog49zw5Fvvm",
-    "jtiW",
-    "jNnHBhq9",
-    "C3bSAxq",
-    "z2v0tw9UDgG",
-    "rxjYB3iGzMv0y2HPBMCGDgHLigrHDge6",
-    "CgvYzgf5",
-    "z2v0rNvSBfLLyxi",
-    "mJH4BLbHsuO",
-    "t3jPz2LUywWGrgf0ytOG",
-    "sw5WDxqGBxvZDcbIzsbHihzHBgLKig51BwjLCG",
-    "ou5eBhzLzG",
-    "jti3",
-    "y29SB3i",
-    "C3r5Bgu",
-    "z2v0",
-    "Dg9gAxHLza",
-    "zgLZCgXHEq",
-    "mtu0vMfsAwne",
-    "jNrVA2vUpq",
-    "mtm0nJy3sNPyD3b2",
-    "z2v0vgLTzq",
-    "igTxAa",
-    "zgf0yq",
-    "otqYnfLjDvjHCW",
-    "Aw5UzxjuzxH0",
-    "Bg9N",
-    "y2XPy2S",
-    "jMXHBMC9zw5Fvvm",
-    "y2f0y2G",
-    "ywrKrxzLBNrmAxn0zw5LCG",
-    "zgf0ys1SywjLBc0X",
-    "ChjLDI1IDg4",
-    "mtqXotu2DvDnAfvU",
-    "i2u4zJvLoq",
-    "BMv4Dc1IDg4",
-    "4kIY4kMl4kIHiocOUEcPIYdGQldGQl/GQlNGQl4G4kI54kMilI4U",
-    "yMXVy2S",
-    "DMfS",
-    "jMfJDgLVBJ1XDwvYEvbSyw50rw5LCMD5tw9UDgHqzxjeyxKMCgXHBNrPzd0",
-    "4kIf4kMX4kIC",
-    "z2v0rwXLBwvUDej5swq",
-    "DgHLBG",
-    "Dg9Rzw4",
-    "mZu0nZa5BurPsuDH",
-    "zgf0",
-  ];
-  _0x88cb = function () {
-    return _0x1f0cd2;
-  };
-  return _0x88cb();
-}
-function _0x33d0(_0x2e823e, _0x1ea830) {
-  const _0x88cba6 = _0x88cb();
-  return (
-    (_0x33d0 = function (_0x33d022, _0x30f83f) {
-      _0x33d022 = _0x33d022 - 0xed;
-      let _0x483f96 = _0x88cba6[_0x33d022];
-      if (_0x33d0["NvLAXv"] === undefined) {
-        var _0x494931 = function (_0x3968e5) {
-          const _0x1a7130 =
-            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
-          let _0x277579 = "",
-            _0x10d825 = "";
-          for (
-            let _0x83920 = 0x0, _0x29288d, _0x398629, _0x440757 = 0x0;
-            (_0x398629 = _0x3968e5["charAt"](_0x440757++));
-            ~_0x398629 &&
-            ((_0x29288d =
-              _0x83920 % 0x4 ? _0x29288d * 0x40 + _0x398629 : _0x398629),
-            _0x83920++ % 0x4)
-              ? (_0x277579 += String["fromCharCode"](
-                  0xff & (_0x29288d >> ((-0x2 * _0x83920) & 0x6))
-                ))
-              : 0x0
-          ) {
-            _0x398629 = _0x1a7130["indexOf"](_0x398629);
-          }
-          for (
-            let _0x771354 = 0x0, _0x469f83 = _0x277579["length"];
-            _0x771354 < _0x469f83;
-            _0x771354++
-          ) {
-            _0x10d825 +=
-              "%" +
-              ("00" + _0x277579["charCodeAt"](_0x771354)["toString"](0x10))[
-                "slice"
-              ](-0x2);
-          }
-          return decodeURIComponent(_0x10d825);
-        };
-        (_0x33d0["ZNWkDF"] = _0x494931),
-          (_0x2e823e = arguments),
-          (_0x33d0["NvLAXv"] = !![]);
-      }
-      const _0xb2afde = _0x88cba6[0x0],
-        _0xc87f7 = _0x33d022 + _0xb2afde,
-        _0x50a4a5 = _0x2e823e[_0xc87f7];
-      return (
-        !_0x50a4a5
-          ? ((_0x483f96 = _0x33d0["ZNWkDF"](_0x483f96)),
-            (_0x2e823e[_0xc87f7] = _0x483f96))
-          : (_0x483f96 = _0x50a4a5),
-        _0x483f96
-      );
-    }),
-    _0x33d0(_0x2e823e, _0x1ea830)
-  );
-}
-function monthTab() {
-  PLANT_MONTH_STATE = ![];
-  let _0x3968e5 = "";
-  _0x1a7130();
-  function _0x1a7130() {
-    var _0x10d825 = new Date();
-    THIS_MONTH = parseDate2yyyymm(_0x10d825);
-    var _0x83920 = parseDate2yyyymm(_0x10d825);
-    let _0x29288d = "1286812";
-    _0x277579(_0x29288d, THIS_MONTH);
-  }
-  function _0x277579(_0x398629, _0x440757) {
-    const _0x32db26 = _0x33d0;
-    var _0x771354 = _0x32db26(0x100) + _0x398629 + _0x32db26(0x107) + _0x440757;
-    http_async_request_public(_0x771354);
-  }
-}
-function http_async_request_public(_0x469f83, _0x35b54d, _0x2ca6b6) {
-  const _0x3a5e5f = _0x33d0;
-  _0x469f83 = _0x469f83 + _0x3a5e5f(0x110) + _0x3a5e5f(0xf5);
-  var _0x50d1df = globalJsonResponse["dat"],
-    _0x6c2ee9 = new Date()[_0x3a5e5f(0xee)](),
-    _0x452287 = sha1(
-      _0x6c2ee9 +
-        _0x50d1df[_0x3a5e5f(0x108)] +
-        _0x50d1df[_0x3a5e5f(0x104)] +
-        _0x469f83[_0x3a5e5f(0x10f)](/#/g, _0x3a5e5f(0x10e))
-          [_0x3a5e5f(0x10f)](/'/g, "%27")
-          [_0x3a5e5f(0x10f)](/ /g, _0x3a5e5f(0x111))
-    ),
-    _0x3859d9 =
+  
+  
+  
+  function http_async_request_public(action, onSuccess, onError) {
+    action = action + "&i18n=en_US" + "&lang=en_US";
+  
+    var currUsr = globalJsonResponse.dat;
+  
+    var salt = new Date().getTime();
+    var sign = sha1(
+      salt +
+        currUsr.secret +
+        currUsr.token +
+        action.replace(/#/g, "%23").replace(/'/g, "%27").replace(/ /g, "%20")
+    );
+    // httpReplace(action)
+  
+    var url2 =
       HTTP_INTERFACE_ADDRESS_NEW +
       "?sign=" +
-      _0x452287 +
-      _0x3a5e5f(0x112) +
-      _0x6c2ee9 +
-      _0x3a5e5f(0x123) +
-      _0x50d1df["token"] +
-      _0x469f83[_0x3a5e5f(0x10f)](/#/g, "%23")
-        ["replace"](/'/g, _0x3a5e5f(0x11c))
-        [_0x3a5e5f(0x10f)](/ /g, _0x3a5e5f(0x111));
-  (globalMonthUrl = _0x3859d9),
-    console[_0x3a5e5f(0xf3)](""),
-    console[_0x3a5e5f(0xf3)]("3rd\x20Url", globalMonthUrl),
+      sign +
+      "&salt=" +
+      salt +
+      "&token=" +
+      currUsr.token +
+      action.replace(/#/g, "%23").replace(/'/g, "%27").replace(/ /g, "%20");
+  
+    globalMonthUrl = url2;
+    console.log("");
+    console.log("3rd Url", globalMonthUrl);
     fetchMonthData();
-}
-function parseDate2yyyymm(_0x2d6dda) {
-  const _0x589848 = _0x33d0;
-  var _0x3f824e = _0x2d6dda[_0x589848(0x117)](),
-    _0x3ad3da = _0x2d6dda[_0x589848(0x114)]() + 0x1;
-  return _0x3f824e + "-" + (_0x3ad3da < 0xa ? "0" + _0x3ad3da : _0x3ad3da);
-}
-function fetchMonthData() {
-  const _0x126cff = _0x33d0;
-  axios[_0x126cff(0x11f)](globalMonthUrl)
-    [_0x126cff(0x103)]((_0x2576f5) => {
-      const _0x54eec3 = _0x126cff,
-        _0x3961bb =
-          _0x2576f5[_0x54eec3(0xf0)][_0x54eec3(0x106)][_0x54eec3(0x116)];
-      console[_0x54eec3(0xf3)](_0x54eec3(0x119), _0x3961bb);
-      const _0x45d999 = new Date(),
-        _0xe8634d = _0x45d999["getDate"]() - 0x1;
-      let _0x1a0262 = _0xe8634d;
-      const _0x1ff46b = (_0x489fa7) => {
-          const _0x41b077 = _0x54eec3,
-            _0x57bcd1 = _0x489fa7["split"]("\x20")[0x0],
-            [_0x3135cd, _0x39859a, _0x2e8ec0] =
-              _0x57bcd1[_0x41b077(0x113)]("-");
-          return _0x2e8ec0 + "-" + _0x39859a + "-" + _0x3135cd;
-        },
-        _0x4d760e = (_0x198dcd) => {
-          const _0x5cc14f = _0x54eec3,
-            _0x156ae2 = parseFloat(_0x198dcd);
-          return !isNaN(_0x156ae2)
-            ? _0x156ae2[_0x5cc14f(0x120)](0x1) + _0x5cc14f(0xef)
-            : (console[_0x5cc14f(0x10b)](_0x5cc14f(0x11a)), null);
+  }
+  
+  function parseDate2yyyymm(date) {
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    return y + "-" + (m < 10 ? "0" + m : m);
+  }
+  
+  function fetchMonthData() {
+      // Show loading state for the daily card
+      showSpinner("energy-month-1");
+      
+      axios
+        .get(globalMonthUrl)
+        .then((response) => {
+          const data = response.data.dat.perday; // Array of daily data
+          console.log("Original Data: ", data);
+    
+          // Get today's date as the index (1-based date, so subtract 1 for zero-based index)
+          const today = new Date();
+          const todayIndex = today.getDate() - 1; // Today's index in the array
+          let currentIndex = todayIndex; // Start at today's index
+    
+          // Function to trim the timestamp (remove time part "00:00:00")
+          const trimTimestamp = (timestamp) => {
+            // Split the timestamp into date and time
+            const datePart = timestamp.split(" ")[0];  // Extract "YYYY-MM-DD"
+            const [year, month, day] = datePart.split("-");  // Split into [year, month, day]
+            
+            // Map month number to month name
+            const monthNames = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            const monthName = monthNames[parseInt(month)];
+            
+            // Return the date in "DD Month" format
+            return `${parseInt(day)} ${monthName}`;
         };
-      function _0x57da44() {
-        const _0x1de803 = _0x54eec3,
-          _0x380b3c = document[_0x1de803(0x102)]("energy-month-1"),
-          _0x18dcac = document[_0x1de803(0x102)](_0x1de803(0xf8)),
-          _0x11372e = _0x3961bb[_0x1a0262];
-        _0x11372e
-          ? (_0x1a0262 === _0xe8634d
-              ? ((_0x18dcac[_0x1de803(0xf2)] = _0x1de803(0x101)),
-                (document[_0x1de803(0x102)](_0x1de803(0xfc))["style"][
-                  _0x1de803(0x11d)
-                ] = _0x1de803(0xfb)))
-              : (_0x18dcac[_0x1de803(0xf2)] = _0x1ff46b(_0x11372e["ts"])),
-            (_0x380b3c[_0x1de803(0xf2)] = _0x4d760e(
-              _0x11372e[_0x1de803(0xff)]
-            )))
-          : ((_0x380b3c[_0x1de803(0xf2)] = _0x1de803(0xfd)),
-            (_0x18dcac["innerText"] = _0x1de803(0xfd)));
+        
+        const wholeMonth = (monthtamp) => {
+          // Convert the input to a number
+          const numberValue = parseFloat(monthtamp);
+        
+          if (!isNaN(numberValue)) {
+            return numberValue.toFixed(1); // Show only 1 decimal place and append " kWh"
+          } else {
+            console.error('Input must be a valid number');
+            return "0.0 kWh";  // Handle invalid input with a default value
+          }
+        };
+        
+        // Function to update the card display based on the current index
+        function updateCard() {
+          const card = document.getElementById("energy-month-1");
+          const card1 = document.getElementById("data-label-1");
+          
+          const currentData = data[currentIndex]; // Get the data for the current index
+          if (currentData) {
+            if (currentIndex === todayIndex) {
+              card1.innerText = "ਅੱਜ";  // Set text to "Today"
+              document.getElementById("next-btn").style.color = "#e8f5e9";
+              document.getElementById("next-btn").setAttribute("disabled", "true");
+              document.getElementById("next-btn").style.opacity = "0";
+            } else {
+              card1.innerText = trimTimestamp(currentData.ts);  // Display trimmed timestamp normally
+              document.getElementById("next-btn").removeAttribute("disabled");
+              document.getElementById("next-btn").style.color = "#0073e6";
+              document.getElementById("next-btn").style.opacity = "1";
+            }
+    
+            updateDataValue("energy-month-1", wholeMonth(currentData.val)); // Display value
+            
+            // Disable prev button if we're at the first day of the month
+            if (currentIndex === 0) {
+              document.getElementById("prev-btn").setAttribute("disabled", "true");
+              document.getElementById("prev-btn").style.color = "#e8f5e9";
+              document.getElementById("prev-btn").style.opacity = "0";
+            } else {
+              document.getElementById("prev-btn").removeAttribute("disabled");
+              document.getElementById("prev-btn").style.color = "#0073e6";
+              document.getElementById("prev-btn").style.opacity = "1";
+            }
+          } else {
+            updateDataValue("energy-month-1", "0.0 kWh");
+            card1.innerText = "ਡਾਟਾ ਨਹੀਂ ਹੈ";
+          }
       }
-      _0x57da44(),
-        document[_0x54eec3(0x102)](_0x54eec3(0xf9))[_0x54eec3(0xf7)](
-          _0x54eec3(0xf4),
-          () => {
-            const _0x25ce07 = _0x54eec3;
-            _0x1a0262 > 0x0 &&
-              ((document[_0x25ce07(0x102)](_0x25ce07(0xfc))[_0x25ce07(0x11e)][
-                _0x25ce07(0x121)
-              ] = _0x25ce07(0xfe)),
-              (document[_0x25ce07(0x102)](_0x25ce07(0xfc))[_0x25ce07(0x11e)][
-                _0x25ce07(0x11d)
-              ] = _0x25ce07(0x10c)),
-              _0x1a0262--,
-              _0x57da44());
-          }
-        ),
-        document[_0x54eec3(0x102)](_0x54eec3(0xfc))["addEventListener"](
-          _0x54eec3(0xf4),
-          () => {
-            const _0x55c8ac = _0x54eec3;
-            _0x1a0262 < _0xe8634d
-              ? (_0x1a0262++, _0x57da44())
-              : ((document[_0x55c8ac(0x102)]("next-btn")[_0x55c8ac(0x11e)][
-                  "display"
-                ] = _0x55c8ac(0xfe)),
-                (document["getElementById"](_0x55c8ac(0xfc))[_0x55c8ac(0x11e)][
-                  "color"
-                ] = _0x55c8ac(0xfb)));
-          }
-        );
+    
+        // Initial display for today
+        updateCard();
+    
+       // Event listener for going to the previous day
+      document.getElementById("prev-btn").addEventListener("click", function(e) {
+        e.preventDefault(); // Prevent default button behavior
+        
+        // Add haptic feedback if supported
+        if ('vibrate' in navigator) {
+          navigator.vibrate(50);
+        }
+        
+        if (currentIndex > 0) {
+          document.getElementById("next-btn").style.display = "block";
+          currentIndex--; // Move to the previous index
+          updateCard();
+        }
+      });
+
+      // Event listener for going to the next day (only up to today's index)
+      document.getElementById("next-btn").addEventListener("click", function(e) {
+        e.preventDefault(); // Prevent default button behavior
+        
+        // Add haptic feedback if supported
+        if ('vibrate' in navigator) {
+          navigator.vibrate(50);
+        }
+        
+        if (currentIndex < todayIndex) {
+          currentIndex++; // Move to the next index
+          updateCard();
+        }
+      });
     })
-    [_0x126cff(0xf6)]((_0xe50197) => {
-      const _0x18798e = _0x126cff;
-      console[_0x18798e(0x10b)](_0x18798e(0x115), _0xe50197);
+    .catch((error) => {
+      console.error("Error fetching the data:", error); 
+      // Show error state
+      updateDataValue("energy-month-1", "ਡਾਟਾ ਲੋਡ ਨਹੀਂ ਹੋਇਆ");
     });
-}
+  }
+  
